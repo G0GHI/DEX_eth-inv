c103ef7837b64036c1f8256d64cf58e8a8ca8bac
Goghi <gorandevetak2004@gmail.com>
2022-07-13 09:12:35 +0200

    added buy function

--- contracts/DEX_eth_inv.sol
+++ contracts/DEX_eth_inv.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title DEX_eth_inv
@@ -11,13 +12,19 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  */
 
 contract DEX_eth_inv {
-    mapping(address => uint256) investedBalanceOf;
+    mapping(address => uint256) public investedBalanceOf;
 
+    AggregatorV3Interface internal ethPriceFeed;
     IERC20 public ethToken;
+    IERC20 public invToken;
     address[] investors;
 
-    constructor(address _ethTokenAddress) {
+    constructor(address _ethTokenAddress, address _invTokenAddress) {
+        ethPriceFeed = AggregatorV3Interface(
+            0x9326BFA02ADD2366b30bacB125260Af641031331
+        ); // for now I will use the kovan testnet priceFeed
         ethToken = IERC20(_ethTokenAddress);
+        invToken = IERC20(_invTokenAddress);
     }
 
     /**
@@ -41,14 +48,29 @@ contract DEX_eth_inv {
         if (checkIfInvestorNotInList(msg.sender)) {
             investors.push(msg.sender);
         }
-        investedBalanceOf(msg.sender) += _amount;
+        investedBalanceOf[msg.sender] += _amount;
     }
 
     /**
-     * @dev takes ETH from the buyer and returns INV
+     * TODO I need to check the decimal of ethPrice, transaction fee
+     * @dev takes ETH from the buyer, calculates the amount of INV to return based on the AMM algorithm and returns INV
+     * @param _amount the amount of eth that a buyer is spending to buy INV
      */
 
-    function buy() public {}
+    function buy(uint256 _amount) public isAmountAllowed(_amount) {
+        ethToken.transferFrom(msg.sender, address(this), _amount);
+
+        uint256 ethPrice = getEthPrice();
+        uint256 liquidityPoolEthBalance = ethToken.balanceOf(address.this);
+        uint256 liquidityPoolInvBalance = invToken.balanceOf(address.this);
+        uint256 liquidityPoolTotal = (ethPrice * liquidityPoolEthBalance)**2;
+        uint256 newAmountOfEthValue = ethPrice *
+            (liquidityPoolEthBalance + _amount);
+        uint256 amountOfInvToReturn = liquidityPoolInvBalance -
+            (liquidityPoolTotal / newAmountOfEthValue);
+
+        invToken.transfer(msg.sender, amountOfInvToReturn);
+    }
 
     /**
      * @dev takes INV from the buyer and returns ETH
@@ -71,5 +93,14 @@ contract DEX_eth_inv {
             }
         }
         return true;
+    }
+
+    /**
+     * @dev function that connects to the chainlink oracle and returns the current eth price
+     */
+
+    function getEthPrice() internal view returns (uint256) {
+        (, int256 price, , , ) = ethPriceFeed.latestRoundData();
+        return uint256(price);
     }
 }
