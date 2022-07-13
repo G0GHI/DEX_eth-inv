7434c92b595fa8da4578fe398a2468355f0f8568
Goghi <gorandevetak2004@gmail.com>
2022-07-13 10:48:42 +0200

    finished for now DEX_eth_inv contract

--- contracts/DEX_eth_inv.sol
+++ contracts/DEX_eth_inv.sol
@@ -7,7 +7,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title DEX_eth_inv
- * @notice the contract that will stor the three main function -- land, buy and sell
+ * @notice the contract that will store the three main functions -- land, buy and sell
  * @author GOGHI
  */
 
@@ -18,6 +18,7 @@ contract DEX_eth_inv {
     IERC20 public ethToken;
     IERC20 public invToken;
     address[] investors;
+    uint256 investedEth;
 
     constructor(address _ethTokenAddress, address _invTokenAddress) {
         ethPriceFeed = AggregatorV3Interface(
@@ -29,12 +30,15 @@ contract DEX_eth_inv {
 
     /**
      * @dev check if the amount is greater than 0 and if the investor can afford it
-     * @param _amount the amount that the investors is investing
+     * @param _amount the amount that the investor is investing
      */
 
     modifier isAmountAllowed(uint256 _amount) {
-        require(_amount > 0);
-        require(ethToken.balanceOf(msg.sender) >= _amount);
+        require(_amount > 0, "Amount must be greater than 0!");
+        require(
+            ethToken.balanceOf(msg.sender) >= _amount,
+            "You don't have enough eth in your wallet!"
+        );
         _;
     }
 
@@ -49,34 +53,57 @@ contract DEX_eth_inv {
             investors.push(msg.sender);
         }
         investedBalanceOf[msg.sender] += _amount;
+        investedEth += _amount;
     }
 
     /**
-     * TODO I need to check the decimal of ethPrice, transaction fee
+     * TODO I need to check the decimal of ethPrice
      * @dev takes ETH from the buyer, calculates the amount of INV to return based on the AMM algorithm and returns INV
+     * than it takes a transaction fee that is 1%;
      * @param _amount the amount of eth that a buyer is spending to buy INV
      */
 
     function buy(uint256 _amount) public isAmountAllowed(_amount) {
+        uint256 liquidityPoolEthBalance = ethToken.balanceOf(address(this));
+        uint256 liquidityPoolInvBalance = invToken.balanceOf(address(this));
+        uint256 liquidityPoolTotal = liquidityPoolEthBalance *
+            liquidityPoolInvBalance;
+
         ethToken.transferFrom(msg.sender, address(this), _amount);
 
-        uint256 ethPrice = getEthPrice();
-        uint256 liquidityPoolEthBalance = ethToken.balanceOf(address.this);
-        uint256 liquidityPoolInvBalance = invToken.balanceOf(address.this);
-        uint256 liquidityPoolTotal = (ethPrice * liquidityPoolEthBalance)**2;
-        uint256 newAmountOfEthValue = ethPrice *
-            (liquidityPoolEthBalance + _amount);
+        uint256 newAmountOfEthToken = liquidityPoolEthBalance + _amount;
         uint256 amountOfInvToReturn = liquidityPoolInvBalance -
-            (liquidityPoolTotal / newAmountOfEthValue);
+            (liquidityPoolTotal / newAmountOfEthToken);
 
         invToken.transfer(msg.sender, amountOfInvToReturn);
+        transactionFee(_amount);
     }
 
     /**
-     * @dev takes INV from the buyer and returns ETH
+     * @dev takes INV from the buyer and returns ETH, no transaction fee here
+     * @param _amount the amount of INV that someone wants to sell
      */
 
-    function sell() public {}
+    function sell(uint256 _amount) public {
+        require(_amount > 0, "Amount must be greater than 0!");
+        require(
+            invToken.balanceOf(msg.sender) >= _amount,
+            "You don't have enought inv in your wallet!"
+        );
+
+        uint256 liquidityPoolEthBalance = ethToken.balanceOf(address(this));
+        uint256 liquidityPoolInvBalance = invToken.balanceOf(address(this));
+        uint256 liquidityPoolTotal = liquidityPoolEthBalance *
+            liquidityPoolInvBalance;
+
+        invToken.transferFrom(msg.sender, address(this), _amount);
+
+        uint256 newAmountOfInvToken = liquidityPoolInvBalance + _amount;
+        uint256 amountOfEthToReturn = liquidityPoolEthBalance -
+            (liquidityPoolTotal / newAmountOfInvToken);
+
+        ethToken.transfer(msg.sender, amountOfEthToReturn);
+    }
 
     /**
      * @dev function that checks if an investor already invested before
@@ -102,5 +129,24 @@ contract DEX_eth_inv {
     function getEthPrice() internal view returns (uint256) {
         (, int256 price, , , ) = ethPriceFeed.latestRoundData();
         return uint256(price);
+    }
+
+    /**
+     * @dev takes 1% of the amount spended and it divides it between me(10% of the fee)
+     * and the investors(they divide the 90% of the fee dependently on how much they invested)
+     * @param _amount the amount that the buyers spended in eth
+     */
+
+    function transactionFee(uint256 _amount) internal {
+        uint256 fee = _amount / 100;
+        uint256 myReward = fee / 10;
+        uint256 investorsReward = (fee / 10) * 9;
+        uint256 x = investorsReward / (investedEth + (investorsReward / 100));
+        for (uint256 i; i < investors.length; i++) {
+            ethToken.transfer(
+                investors[i],
+                (x * investedBalanceOf[investors[i]])
+            );
+        }
     }
 }
